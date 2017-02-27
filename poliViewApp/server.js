93af4cbb80e3c3d83192c4206c328f85dfdce8ca
Justin Samuels <blizm87@gmail.com>
2017-02-27 01:41:59 -0800

    Setup server to render, and installed forgotten dependencies

--- poliViewApp/server.js
+++ poliViewApp/server.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const path = require('path');
+
+const app = express();
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 // CONFIG
@@ -12,11 +16,12 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
-app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: true}));
+// app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: true}));
+
 // ROUTES
 
 // SOCKETS
-const sockets = require('./routes/sockets')(io);
+// const sockets = require('./routes/sockets')(io);
 
 // SERVER
 const port = process.env.PORT || 3000;
