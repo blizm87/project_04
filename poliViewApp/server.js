43800ac6b14de8889516bc486b8e938c1eab57e2
Justin Samuels <blizm87@gmail.com>
2017-02-27 11:18:41 -0800

    Recorrected file format for entire project, split react logic into reactPoliView, and authentication in to poliviewApp

--- poliViewApp/server.js
+++ poliViewApp/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const hbs = require('express-handlebars');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const path = require('path');
@@ -11,20 +12,24 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 // CONFIG
+
 require('./db/config');
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 // app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: true}));
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, 'views/layouts/')}));
+app.set('view engine', 'hbs');
 
 // ROUTES
+app.use('/', require('./routes/index'));
 
 // SOCKETS
 // const sockets = require('./routes/sockets')(io);
 
 // SERVER
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 http.listen(port, () => {
   console.log('Listening on Port: ' + port);
---
+++ reactPoliViewApp/src/MainPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../public/css/MainPage.css';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <h1>Justin's React</h1>
+    );
+  }
+}
+
+export default MainPage;
